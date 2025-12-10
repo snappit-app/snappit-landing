3f56bf44e749569b81233fdf2263d2c7ff8c19eb
atiurin <pinkcolorrrs@gmail.com>
2025-12-10 10:40:13 +0700

    Init linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,63 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import prettier from "eslint-plugin-prettier";
+import importSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import react from "eslint-plugin-react";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const config = tseslint.config(
+ { ignores: [".next", "out", "build", "next-env.d.ts"] },
+ {
+  extends: [
+   js.configs.recommended,
+   ...tseslint.configs.recommended,
+   prettierConfig,
+  ],
+  files: ["**/*.{ts,tsx,mts}"],
+  languageOptions: {
+   ecmaVersion: 2020,
+   globals: globals.browser,
+   parserOptions: {
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: import.meta.dirname,
+   },
+  },
+  plugins: {
+   react,
+   "react-hooks": reactHooks,
+   prettier,
+   "import-sort": importSort,
+  },
+  settings: {
+   react: {
+    version: "detect",
+   },
+  },
+  rules: {
+   ...react.configs.recommended.rules,
+   ...reactHooks.configs.recommended.rules,
+   "react/react-in-jsx-scope": "off",
+   "import-sort/imports": "warn",
+   "import-sort/exports": "warn",
+   "@typescript-eslint/no-unused-vars": [
+    "warn",
+    {
+     argsIgnorePattern: "^_",
+     varsIgnorePattern: "^_",
+    },
+   ],
+   "prettier/prettier": [
+    "error",
+    {
+     printWidth: 100,
+     tabWidth: 2,
+     singleQuote: false,
+    },
+   ],
+  },
+ },
+);
 
-export default eslintConfig;
+export default config;
